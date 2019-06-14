980ba675c4c8e99105bfb601f25c7a0efc5f02bf
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-14 11:21:37 -0700

    Add files via upload

---
+++ trycatch drill/trycatch drill/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace trycatch_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbers = { 2, 4, 6, 8, 10, 12, 14 };
+                Console.WriteLine("choose a number to divide the numbers in the list by.");
+                int divider = Convert.ToInt32(Console.ReadLine());
+
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int equals = numbers[i] / divider;
+                    Console.WriteLine(numbers[i] + " divided by " + divider + " is " + equals);
+                }
+            }
+
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error. Type a whole number.");
+                return;
+            }
+
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error. Cannot divide by zero.");
+                return;
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            finally
+            {
+                Console.WriteLine("The program has emerged from the try/catch block and continued on with the program.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
