a1a67355e0c436280da42734e0cb2fa277693140
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-15 15:39:30 -0700

    Add files via upload

---
+++ Optional method drill/Optional method drill/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Optional_method_drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            numbers numbers = new numbers();
+
+            int firstNum = 0;
+            int secondNum = 0;
+            string Input = "";
+
+            bool correctNum = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Type a whole number.");
+                    firstNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("The second whole number is optional. Press enter to continue.");
+                    Input = Console.ReadLine();
+                    if (Input != "") Convert.ToInt32(Input); 
+
+                    correctNum = true;
+                }
+                catch
+                {
+                    correctNum = false;
+                }
+
+
+            } while (!correctNum);
+
+
+
+            if (Input == "")
+            {
+                Console.WriteLine("Equals: {0}", numbers.Average(firstNum));
+            }
+            else
+            {
+                secondNum = Convert.ToInt32(Input);
+                Console.WriteLine("Equals: {0}", numbers.Average(firstNum, secondNum));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Optional method drill/Optional method drill/numbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Optional_method_drill
+{
+    class numbers
+    {
+        public int Average(int num1, int num2 = 2)
+        {
+            return (num1 - num2) * 4;
+        }
+    }
+}
