4d2285f96673c648648e66c3db535e7c3d9a4f3a
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-14 17:57:21 -0700

    Add files via upload

---
+++ Methods drill/Methods drill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Methods_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("type a whole number.");
+        int number = Convert.ToInt32(Console.ReadLine());
+        math_operations.Method(number);
+
+            Console.WriteLine("type a decimal.");
+
+
+            float number2 = float.Parse(Console.ReadLine());
+        math_operations.Method(number2);
+        Console.WriteLine("type an integer.");
+
+
+            string number3 = Console.ReadLine();
+        math_operations.Method(number3);
+        Console.ReadLine();
+    }
+    }
+}
---
+++ Methods drill/Methods drill/math operations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods_drill
+{
+    public class math_operations
+    {
+        public static void Method(int Number)
+        {
+            int equals = Number - 4;
+            Console.WriteLine(Number + " minus 4 = " + equals);
+        }
+        public static void Method(float Fl)
+        {
+            float equals2 = Fl * 7;
+            Console.WriteLine(Fl + " multiplied by  7 = " + equals2);
+        }
+        public static void Method(string Str)
+        {
+            try
+            {
+                int num = Convert.ToInt32(Str);
+                int equals3 = num + 10;
+                Console.WriteLine(num + " plus 10 = " + equals3);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Incorrect.");
+                return;
+            }
+        }
+    }
+}
