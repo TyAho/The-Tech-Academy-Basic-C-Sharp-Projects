dedab711b3c8d8b5e8ba836e2c6ce4e493d7df8a
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-15 13:50:54 -0700

    Add files via upload

---
+++ Class drill/Class drill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Class_drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            inputs in1 = new inputs();
+            decimal firstNum = 6.78M;
+            Console.WriteLine(in1.DoMath(firstNum));
+
+            inputs in2 = new inputs();
+            int secondNum = 8;
+            Console.WriteLine(in2.DoMath(secondNum));
+
+            inputs in3 = new inputs();
+            string thirdNum = "10";
+            Console.WriteLine(in3.DoMath(thirdNum));
+
+
+            Console.ReadLine();
+        }
+    }
+}
