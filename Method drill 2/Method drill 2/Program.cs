2452b2e8e0e41ff5d403aa842e3c17cf1470fd6f
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-14 23:56:14 -0700

    Add files via upload

---
+++ Method drill 2/Method drill 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_drill_2
+{
+    class Program
+    {
+            public static void Main()
+            {
+                int number1 = 10;
+
+
+                int number2 = 20;
+
+                Number.Drill(number1, number2);
+            }
+        }
+        class Number
+        {
+
+
+            public static void Drill(int construct1, int construct2)
+            {
+                int equals = construct1 - 8;
+                Console.WriteLine(equals);
+                Console.WriteLine(construct2);
+                Console.ReadLine();
+          }
+     }
+}
