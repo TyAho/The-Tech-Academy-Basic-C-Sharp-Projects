4e5706fd88f71b2763b9caea0aa39cc37d5f8fac
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-15 18:36:28 -0700

    Add files via upload

---
+++ three methods drill/three methods drill/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace three_methods_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a whole number.");
+            bool correctInput = false;
+            int input = 0;
+            while (!correctInput)
+            {
+                try
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                    correctInput = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Error. Please enter a whole number.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine("1st operation equals:");
+            input = operations.Add(input);
+            Console.WriteLine(Convert.ToString(input));
+
+            Thread.Sleep(100);
+
+            Console.WriteLine("2nd operation equals:");
+            input = operations.Third(input);
+            Console.WriteLine(Convert.ToString(input));
+
+            Thread.Sleep(100);
+
+            Console.WriteLine("3rd operation equals:");
+            input = operations.random(input);
+            Console.WriteLine(Convert.ToString(input));
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ three methods drill/three methods drill/operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace three_methods_drill
+{
+    static public class operations
+    {
+        static public int Add(int input)
+        {
+            return input + 6;
+        }
+
+        static public int Third(int input)
+        {
+            return (input / 3);
+        }
+
+        static public int random(int input)
+        {
+            Random random = new Random();
+            return input + random.Next(-4, 4);
+        }
+    }
+}
