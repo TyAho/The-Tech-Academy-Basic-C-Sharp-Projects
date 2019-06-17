eef5889926bddc0364039f38729dd73544a4ab8b
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-17 16:39:25 -0700

    Add files via upload

---
+++ IQuittable drill/IQuittable drill/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace IQuittable_drill
+{
+    public class Employee : Person, IQuittable
+    {
+        public void Quit()
+        {
+            IQuittable quittable = new Employee();
+            Console.WriteLine("Player has quit the game.");
+            Console.ReadLine();
+        }
+    }
+}
