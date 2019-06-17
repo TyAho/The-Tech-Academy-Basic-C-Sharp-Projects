794dff4e5914bbe6fab574c332065aedb87db6a9
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-16 21:01:40 -0700

    Add files via upload

---
+++ inheritance drill/inheritence drill/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace inheritance_drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee sample = new Employee() { FirstName = "Sample", LastName = "Student", ID = 1 };
+
+            Employee different = new Employee() { FirstName = "Robert", LastName = "Smith", ID = 2 };
+
+            sample.SayName();
+            different.SayName();
+
+           
+
+
+            
+        }
+    }
+}
