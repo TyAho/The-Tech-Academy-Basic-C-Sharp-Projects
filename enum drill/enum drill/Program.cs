3fbc5c80f33ee4ba9810bb14d7cb3672a5e56e41
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-18 15:30:54 -0700

    Add files via upload

---
+++ enum drill/enum drill/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please type a day of the week.");
+            Week day = 0;
+            bool CorrectDay = false;
+
+
+            while (!CorrectDay)
+            {
+
+
+                try
+                {
+                    string input = Console.ReadLine();
+                    
+                    
+
+                             
+                    CorrectDay = Enum.TryParse(input, out day);
+                    if (!CorrectDay) throw new Exception();
+                }
+
+
+                catch
+                {
+                    CorrectDay = false;
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+            }
+
+            Console.WriteLine(day);
+
+            Console.ReadLine();
+        }
+    }
+    public enum Week
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
+
