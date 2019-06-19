9fa589e1c04275f649411de4eda566b072f16396
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-18 18:57:24 -0700

    Add files via upload

---
+++ structs drill/structs drill/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace structs_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 10;
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+
+    struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
+    
+
