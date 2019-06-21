5ef65eeea155b3b30b30e2766b19e4f00ade3ac8
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-20 18:10:36 -0700

    Add files via upload

---
+++ Text file drill/Text file drill/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Text_file_drill
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Enter a number");
+            bool validInput = false;
+            string input = "";
+            while (!validInput)
+            {
+                try
+                {
+                    input = Console.ReadLine();
+                    Convert.ToInt32(input);
+                    validInput = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Error. Please enter a whole number!");
+                    validInput = false;
+                }
+            }
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Public\file io\number.txt", false))
+            {
+                file.WriteLine(input);
+            }
+
+            string fileText = File.ReadAllText(@"C:\Users\Public\file io\number.txt");
+            Console.WriteLine("The number you entered was {0}", fileText);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
