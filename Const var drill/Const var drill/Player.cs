7a3653711c6fe248c413c6aee08ae919c40d8261
TyAho <49496998+TyAho@users.noreply.github.com>
2019-06-21 13:12:39 -0700

    Add files via upload

---
+++ Const var drill/Const var drill/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Const_var_drill
+{
+    class Player
+    {
+        public int id;
+        public string name;
+        public Player() : this(0, "") { }
+        public Player(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public Player(int id) : this(id, "") { }
+        public Player(string name) : this(0, name) { }
+    }
+}
---
+++ Const var drill/Const var drill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const_var_drill
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string Game = "Overwatch";
+            var developmentYear = 2016;
+            Player player1 = new Player(1324, "Roadhog");
+            Player player2 = new Player("Junkrat");
+            Console.WriteLine("Welcome to " + Game + "\nA team based first person shooter developed in " + developmentYear + ". These are your teammates. \n" + player1.name + " & " + player2.name + "\nGood luck!");
+            Console.ReadLine();
+        }
+    }
+}
